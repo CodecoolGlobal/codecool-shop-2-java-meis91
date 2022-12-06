8c4b13a738ec8082bdbca7acfd155d5e89348de1
Katharina Vogler <kvog82@gmail.com>
2022-12-06 13:14:38 +0100

    bugfix product image

--- src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -27,7 +27,7 @@ public class CartDaoMem implements CartDao {
 
     @Override
     public void add(Product product) {
-        product.setId(data.size() + 1);
+        product.setId(product.getId());
         data.add(product);
     }
 
