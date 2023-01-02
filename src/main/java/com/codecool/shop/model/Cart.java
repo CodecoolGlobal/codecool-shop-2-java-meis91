e366e62e8afe6214d32b3210b8931c6e667234b6
meis91 <adrian.meis91@gmail.com>
2023-01-02 20:21:55 +0100

    started new cart design, read commetn navbar.css

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,13 +1,15 @@
 package com.codecool.shop.model;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cart {
 
 
     private Customer customer;
-    private List<Product> shoppingCart;
+    private Map<Product, Integer> shoppingCart = new HashMap<>();
 
     public Cart() {
 
@@ -21,5 +23,16 @@ public class Cart {
         this.customer = customer;
     }
 
+    public void addProductToCart(Product product){
+        int amount = 1;
+        if(shoppingCart.containsKey(product)){
+            amount = shoppingCart.get(product) + 1;
+            shoppingCart.put(product, amount);
+        }
+        shoppingCart.put(product, amount);
+    }
 
+    public Map<Product, Integer> getShoppingCart() {
+        return shoppingCart;
+    }
 }
